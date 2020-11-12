b2226d259ef8df389774f59c3f2c2336c3a1d953
atmosmps <atmos.mps@gmail.com>
2020-11-12 00:45:13 -0300

    resolve challenge 4

--- pacote-desafios-pythonicos/04_mix_up.py
+++ pacote-desafios-pythonicos/04_mix_up.py
@@ -9,12 +9,27 @@ Exemplo:
     'mix', 'pod' -> 'pox mid'
     'dog, 'dinner' -> 'dig donner'
 
-Assuma que a e b tem tamanho 2 ou maior.
+Old rule: Assuma que a e b tem tamanho 2 ou maior.
+New rule: Assuma que a e b são strings não vazias e não Nulas.
 """
 
+
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    # Solution 1
+    # mix_first_a = a.replace(a[:2], b[:2])
+    # mix_first_b = b.replace(b[:2], a[:2])
+    #
+    # mix_up_result = '{} {}'.format(mix_first_a, mix_first_b)
+    #
+    # return mix_up_result
+
+    # Solution 2
+    # return '{} {}'.format(a.replace(a[:2], b[:2]), b.replace(b[:2], a[:2]))
+
+    # Solution 3
+    return ''.join('Ambas as strings estão vazias ou são Nulas, ou apenas uma delas está vazia, ou é nula.')\
+        if (((a == '') or (b == '')) or ((a == None) or (b == None)))\
+        else ' '.join((a.replace(a[0:2], b[:2]), b.replace(b[0:2], a[:2])))
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -42,3 +57,10 @@ if __name__ == '__main__':
     test(mix_up, ('dog', 'dinner'), 'dig donner')
     test(mix_up, ('gnash', 'sport'), 'spash gnort')
     test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
+    test(mix_up, ('a', 't'), 't a')
+    test(mix_up, ('at', 'm'), 'm at')
+    test(mix_up, ('a', 'tm'), 'tm a')
+    test(mix_up, ('', ''), 'Ambas as strings estão vazias ou são Nulas, ou apenas uma delas está vazia, ou é nula.')
+    test(mix_up, ('a', ''), 'Ambas as strings estão vazias ou são Nulas, ou apenas uma delas está vazia, ou é nula.')
+    test(mix_up, ('', 't'), 'Ambas as strings estão vazias ou são Nulas, ou apenas uma delas está vazia, ou é nula.')
+    test(mix_up, (None, None), 'Ambas as strings estão vazias ou são Nulas, ou apenas uma delas está vazia, ou é nula.')
