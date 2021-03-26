24a6386b68a0da55bc2282fc20d566097b100565
atmosmps <atmos.mps@gmail.com>
2021-03-25 23:42:04 -0300

    test: add new case Hometest in project

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def setUp(self) -> None:
+        self.response = self.client.get('/')
+
+    def test_should_return_200_status_code_when_root_route_is_accessed(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_should_return_the_used_template_in_home_page(self):
+        self.assertTemplateUsed(self.response, 'index.html')
