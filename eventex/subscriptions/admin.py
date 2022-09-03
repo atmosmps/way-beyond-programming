a831badee535e0590e723247106095d65a657057
atmosmps <atmos.mps@gmail.com>
2022-09-03 16:33:02 -0300

    chore: add action mark_as_paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -23,9 +23,21 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     # list_filter = permite filtrar a lista facilmente
     list_filter = ("created_at", "paid")
 
+    actions = ["mark_as_paid"]
+
     def subscribed_today(self, obj):
         return obj.created_at == now().date()
 
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+        if count == 1:
+            msg = "{count} inscriação foi marcada como paga."
+        else:
+            msg = "{count} inscrições foram marcadas como pagas."
+
+        self.message_user(request=request, message=msg.format(count=count))
+
+    mark_as_paid.short_description = "Marcar como pago"
     subscribed_today.short_description = "inscrito hoje?"
     subscribed_today.boolean = True
 
---
+++ eventex/subscriptions/tests/test_admin.py
@@ -0,0 +1,50 @@
+from email import message
+from unittest.mock import Mock
+from urllib import request
+
+from django.test import TestCase
+
+from eventex.subscriptions.admin import Subscription, SubscriptionModelAdmin, admin
+
+
+class SubscriptionModelAdminTest(TestCase):
+    def setUp(self) -> None:
+        Subscription.objects.create(
+            name="Some User Name",
+            cpf="12345678901",
+            email="some@email.com",
+            phone="12-91234-5678",
+        )
+        self.model_admin = SubscriptionModelAdmin(
+            model=Subscription, admin_site=admin.site
+        )
+
+        return super().setUp()
+
+    def _call_action(self):
+        queryset = Subscription.objects.all()
+        original_message = SubscriptionModelAdmin.message_user
+        mock = Mock()
+
+        SubscriptionModelAdmin.message_user = mock
+
+        self.model_admin.mark_as_paid(None, queryset)
+        SubscriptionModelAdmin.message_user = original_message
+        return mock
+
+    def test_has_action(self):
+        """Action mark_as_paid must be installed."""
+        self._call_action()
+        self.assertIn("mark_as_paid", self.model_admin.actions)
+
+    def test_mark_all(self):
+        """It's should mark all selected subscriptions as paid."""
+        self._call_action()
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+    def test_message_admin(self):
+        """It's should send a message to the user"""
+        mock = self._call_action()
+        mock.assert_called_once_with(
+            request=None, message="1 inscriação foi marcada como paga."
+        )
