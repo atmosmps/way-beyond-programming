502cf71c73c1cb7228dea5035ea111a5d8085334
atmosmps <atmos.mps@gmail.com>
2022-12-18 16:44:46 -0300

    chore: create contact feature for manage contacts

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -21,11 +21,26 @@ class Speaker(models.Model):
 
 
 class Contact(models.Model):
+    EMAIL = "E"
+    PHONE = "P"
+    KINDS = (
+        (EMAIL, "Email"),
+        (PHONE, "Telefone"),
+    )
     # é melhor usar a definição de um relacionamento definindo o modelo
     # string, dessa maneira o Django internamente consegue definir qual a
     # relação interna entre os models, sem a necessidade de ficar realizando
     # imports entre os módulos, isso é importante principalmente entre modelos
     # que estão em módulos/apps diferentes pois evita imports circulares.
-    speaker = models.ForeignKey("Speaker", on_delete=models.CASCADE)
-    kind = models.CharField(max_length=1)
-    value = models.CharField(max_length=255)
+    speaker = models.ForeignKey(
+        "Speaker", on_delete=models.CASCADE, verbose_name="palestrante"
+    )
+    kind = models.CharField(verbose_name="tipo", max_length=1, choices=KINDS)
+    value = models.CharField(verbose_name="valor", max_length=255)
+
+    class Meta:
+        verbose_name = "Contato"
+        verbose_name_plural = "Contatos"
+
+    def __str__(self):
+        return self.value
--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -1,18 +1,47 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from eventex.core.models import Contact, Speaker
 
 
 class ContactModelTest(TestCase):
-    def test_create(self):
-        speaker = Speaker.objects.create(
+    def setUp(self) -> None:
+        self.speaker = Speaker.objects.create(
             name="Some Name", slug="some-name", photo="https://somelink/pic"
         )
 
-        contact = Contact.objects.create(
-            speaker=speaker,
-            kind="E",
+    def test_email(self):
+        Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
             value="some@email.com",
         )
 
         self.assertTrue(Contact.objects.exists())
+
+    def test_phone(self):
+        Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.PHONE,
+            value="98988989898",
+        )
+
+        self.assertTrue(Contact.objects.exists())
+
+    def test_model_choices(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind="A",
+            value="B",
+        )
+
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_str(self):
+        contact = Contact(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value="some@email.com",
+        )
+        self.assertEqual("some@email.com", str(contact))
