b5fc30ba987a21ef8e6be6bc1daba0165af373ce
atmosmps <atmos.mps@gmail.com>
2023-07-05 22:13:03 -0300

    refactor: subscription create view finish

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,11 +1,7 @@
 from django.conf import settings
 from django.core import mail
-from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
-from django.views import View
-from django.views.generic import DetailView
-from django.views.generic.base import TemplateResponseMixin
-from django.views.generic.edit import FormMixin
+from django.views.generic import CreateView, DetailView
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
@@ -19,52 +15,28 @@ que vai tratar o dados e fazer as devidas validações.
 """
 
 
-def _send_email(subject, from_, to, template_name, context):
-    message = render_to_string(template_name=template_name, context=context)
-    mail.send_mail(
-        subject=subject, message=message, from_email=from_, recipient_list=[from_, to]
-    )
-
-
-class SubscriptionCreate(TemplateResponseMixin, View):
-    template_name = "subscriptions/subscription_form.html"
+class SubscriptionCreate(CreateView):
+    model = Subscription
     form_class = SubscriptionForm
 
-    def get_form(self):
-        if self.request.method == "POST":
-            return self.form_class(self.request.POST)
-        return self.form_class()
-
-    def get_context_data(self, **kwargs):
-        context = dict(kwargs)
-        context.setdefault("form", self.get_form())
-        return context
-
-    def form_invalid(self, form):
-        return self.render_to_response(context=self.get_context_data(form=form))
-
     def form_valid(self, form):
-        subscription = form.save()
+        response = super().form_valid(form)
+        self.send_email()
+        return response
 
-        _send_email(
-            template_name="subscriptions/subscription_email.txt",
-            context={"subscription": subscription},
-            subject="Confirmação de Inscrição",
-            from_=settings.DEFAULT_FROM_EMAIL,
-            to=subscription.email,
+    def send_email(self):
+        """Send subscription email"""
+
+        subject = "Confirmação de Inscrição"
+        from_ = settings.DEFAULT_FROM_EMAIL
+        to = self.object.email
+        template_name = "subscriptions/subscription_email.txt"
+        context = {"subscription": self.object}
+
+        body = render_to_string(template_name=template_name, context=context)
+        mail.send_mail(
+            subject=subject, message=body, from_email=from_, recipient_list=[from_, to]
         )
-
-        return HttpResponseRedirect(subscription.get_absolute_url())
-
-    def get(self, *args, **kwargs):
-        return self.render_to_response(context=self.get_context_data())
-
-    def post(self, *args, **kwargs):
-        form = self.get_form()
-
-        if not form.is_valid():
-            return self.form_invalid(form)
-        return self.form_valid(form)
 
 
 new = SubscriptionCreate.as_view()
